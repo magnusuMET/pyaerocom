5702be9f3506328b1bd35e6931be92bcb4262be6
thorbjoernl (Thorbjørn) <51087536+thorbjoernl@users.noreply.github.com>
2025-01-07 08:32:49 +0000

    Attempt to fix flaky test

--- pyaerocom/io/mscw_ctm/reader.py
+++ pyaerocom/io/mscw_ctm/reader.py
@@ -572,7 +572,7 @@ class ReadMscwCtm(GriddedReader):
                     file_end_date = nc["time"][:].data.max()
 
                 start_date = min([x for x in [start_date, file_start_date] if x is not None])
-                end_date = max([x for x in [file_end_date, file_end_date] if x is not None])
+                end_date = max([x for x in [end_date, file_end_date] if x is not None])
 
             logger.info(f"******* Found enddate, start_date: {end_date}, {start_date}")
             logger.info(f"{(end_date - start_date) / np.timedelta64(1, 'h')}")
